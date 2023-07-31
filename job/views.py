648caa5856578eef2e5de82ef6f55635290795f9
AshVas <99770789+AshVas@users.noreply.github.com>
2023-07-31 01:49:06 -0400

    first commit

---
+++ job/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from job.models import Project
+# from rest_framework import generics
+
+# Create your views here.
+
+def index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'index.html', context)
+
+def detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'detail.html', context)
